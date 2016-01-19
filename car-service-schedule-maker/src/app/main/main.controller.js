0b0dab1aa0e37ae7129d9e64d7c6eaa2b6e32e9c
Pedro Mass <drom296@gmail.com>
2016-01-19 13:35:38 -0500

    initial commit

---
+++ car-service-schedule-maker/src/app/main/main.controller.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('carServiceScheduleMaker')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+
+    }
+  }
+})();
